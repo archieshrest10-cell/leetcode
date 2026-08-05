7e6f64161424478dd984cb9b630127f5844260ac
archieshrest10-cell <archieshrest10@gmail.com>
2026-08-06 01:20:49 +0530

    Time: 20 ms (10.7%), Space: 42.9 MB (8.44%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int mySqrt(int x) {
+     
+        for (int i = 1; i <= x; i++) {
+            if ((long) i * i > x) {
+                return i - 1;
+            }
+        }
+
+        return x;
+    }
+
+}
